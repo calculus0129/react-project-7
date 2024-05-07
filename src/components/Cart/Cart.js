fa40be154e7203c3714923acbaa85e687cc2c67c
Jaehyun Bhang <ryanb@Jaehyunui-MacBookPro.local>
2024-05-07 20:10:11 +0900

    refactor: work with http requests and notifications

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,19 +1,53 @@
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
+import { actions } from '../../store';
 
 const Cart = (props) => {
-  const cart = useSelector(state=>state.item);
+  const dispatch = useDispatch();
+  const cart = useSelector(state => state.item);
   const items = new Array(...cart.items);
 
   useEffect(() => {
-    fetch('https://redux-firebase-27caf-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart),
-    });
-  }, [cart]);
+    const sendCartData = async () => {
+      dispatch(
+        actions.ui.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
+      );
+      const response = await fetch('https://redux-firebase-27caf-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      });
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+
+      dispatch(
+        actions.ui.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        })
+      );
+      // const responseData = await response.json();
+    };
+    sendCartData().catch((error) => {
+      dispatch(
+        actions.ui.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sent cart data failed!',
+        })
+      );
+    })
+
+  }, [cart]); // to get rid of warnings, include the dispatch in the dependency array.
+  
 
   // console.log(items);
   // HOW???
@@ -21,7 +55,7 @@ const Cart = (props) => {
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        {items.map((arg) => <CartItem key={arg.id} item={arg}/>)}
+        {items.map((arg) => <CartItem key={arg.id} item={arg} />)}
       </ul>
     </Card>
   );
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,11 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
 const uiSlice = createSlice ({
     name: "ui",
-    initialState : { showCart: false },
+    initialState : { showCart: false, notification: null },
     reducers: {
         toggle(state) {
             state.showCart = !state.showCart;
-        }
+        },
+        // Show the HTTP request notifications
+        showNotification(state, action) {
+            state.notification = {
+                status: action.payload.status,
+                title: action.payload.title,
+                message: action.payload.message,
+            };
+        },
     }
 });
 
