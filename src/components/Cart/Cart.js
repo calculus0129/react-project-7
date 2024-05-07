7ce6d80755bc83d3e474c22510767f467269014f
Jaehyun Bhang <ryanb@Jaehyunui-MacBookPro.local>
2024-05-07 10:54:28 +0900

    feat: hook the application together with Firebase server!

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,10 +2,20 @@ import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 const Cart = (props) => {
-  const items = new Array(...useSelector(state=>state.item.items));
-  console.log(items);
+  const cart = useSelector(state=>state.item);
+  const items = new Array(...cart.items);
+
+  useEffect(() => {
+    fetch('https://redux-firebase-27caf-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
+  // console.log(items);
   // HOW???
   return (
     <Card className={classes.cart}>
