c6da373c99dfb27ea1a1bf5bef92ae902e65777e
Jaehyun Bhang <ryanb@Jaehyunui-MacBookPro.local>
2024-05-07 20:28:52 +0900

    move the useEffect code to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
+import { actions } from './store';
 
 function App() {
+  const dispatch = useDispatch();
+  const cart = useSelector(state => state.item);
   const showCart = useSelector(state=>state.ui.showCart);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        actions.ui.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
+      );
+      const response = await fetch('https://redux-firebase-27caf-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      });
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+
+      dispatch(
+        actions.ui.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        })
+      );
+      // const responseData = await response.json();
+    };
+    sendCartData().catch((error) => {
+      dispatch(
+        actions.ui.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sent cart data failed!',
+        })
+      );
+    })
+
+  }, [cart, dispatch]); // to get rid of warnings, include the dispatch in the dependency array, although it is ensured by the react not to be modified.
+
   return (
     <Layout>
       {showCart && <Cart />}
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,54 +1,10 @@
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { actions } from '../../store';
+import { useSelector } from 'react-redux';
 
 const Cart = (props) => {
-  const dispatch = useDispatch();
-  const cart = useSelector(state => state.item);
-  const items = new Array(...cart.items);
-
-  useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        actions.ui.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!',
-        })
-      );
-      const response = await fetch('https://redux-firebase-27caf-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
-        method: 'PUT',
-        body: JSON.stringify(cart),
-      });
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-
-      dispatch(
-        actions.ui.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!',
-        })
-      );
-      // const responseData = await response.json();
-    };
-    sendCartData().catch((error) => {
-      dispatch(
-        actions.ui.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sent cart data failed!',
-        })
-      );
-    })
-
-  }, [cart]); // to get rid of warnings, include the dispatch in the dependency array.
-  
-
+  const items = new Array(...useSelector(state => state.item.items));
   // console.log(items);
   // HOW???
   return (
